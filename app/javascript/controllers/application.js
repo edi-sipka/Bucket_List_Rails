319b1c6083730d269fedab4712318ffa196b79e2
edi-sipka@gmail.com <edisipka@gmail.com>
2023-02-16 13:28:51 +0100

    Initial styling

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,18 +6,4 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
-window.addEventListener("turbolinks:load", () => {
-    hideNotice();
-  });
-  
-  function hideNotice() {
-    const notification = document.querySelector('.notification')
-    if (notification) {
-      setInterval(function() {
-        notification.classList.add('fade');
-      }, 5000);
-    }
-  }
-  
-  console.log("test")
 export { application }
